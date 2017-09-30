8ec4222949dbe70d9d4d557ed871e40e0035f308
unknown <xxx419168445@gmail.com>
2017-09-30 09:43:31 -0700

    trying to figure out how to make play take turns

--- js/app.js
+++ js/app.js
@@ -2,5 +2,25 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+  var switchTurn = 1;
+  var gameEnd = 1;
+
+  function playerPlay(player) {
+  $('.box').one('click',function(player){
+    $(this).append('<img class = "player" src="player.jpg">');
+    })
+  };
+
+  if (switchTurn === 1) {
+    playerPlay(d1);
+    switchTurn = 2;
+  }
+  if (switchTurn === 2) {
+    playerPlay(d2);
+    switchTurn = 1;
+  }
+
+
+ console.log(switchTurn);
 
 });
